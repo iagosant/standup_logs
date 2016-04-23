0aea0375480e6407cadcc2d805feec642318fd84
Santiago Garza <sgarzaceja@me.com>
2016-04-23 13:55:28 -0400

    added email to user model and to views, also added mailer need to finish send email

---
+++ app/mailers/weekly_update.rb
@@ -0,0 +1,65 @@
+class WeeklyUpdate < ApplicationMailer
+  before_action :set_date
+  default from: 'sgarzaceja@gmail.com'
+
+  # Subject can be set in your I18n file at config/locales/en.yml
+  # with the following lookup:
+  #
+  #   en.weekly_update.wips.subject
+  #
+
+  def set_date
+
+    @dow = Time.now.strftime("%I%M")
+    if @dow  == "0147"
+      wips
+    end
+
+  end
+
+  def all_users
+
+    @users = User.all
+
+  end
+
+  def wips
+
+    User.find_each do |user|
+    @email = user.email
+    @blockers = user.blockers.last
+    WeeklyUpdate.deliver_now
+    mail to: @email, subject: "these are your blockers"
+    end
+
+  end
+
+  # Subject can be set in your I18n file at config/locales/en.yml
+  # with the following lookup:
+  #
+  #   en.weekly_update.completeds.subject
+  #
+  def completeds
+    @greeting = "Hi"
+
+    mail to: "to@example.org"
+  end
+
+  # Subject can be set in your I18n file at config/locales/en.yml
+  # with the following lookup:
+  #
+  #   en.weekly_update.blockers.subject
+  #
+  def blockers
+    @greeting = "Hi"
+
+    mail to: "to@example.org"
+  end
+
+  private
+    # Use callbacks to share common setup or constraints between actions.
+    def set_user
+      @user = User.find(params[:id])
+    end
+
+end
