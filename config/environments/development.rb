e1206603faf19965eac1756b955d8099f43ff22e
admin <admin@iMac-2.local>
2016-06-07 16:49:40 -0400

    change in development and production

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -1,16 +1,33 @@
 Rails.application.configure do
   # Settings specified here will take precedence over those in config/application.rb.
 
+  # config.action_mailer.delivery_method = :smtp
+  # # SMTP settings for gmail
+  # config.action_mailer.smtp_settings = {
+  #  :address              => "smtp.gmail.com",
+  #  :port                 => 587,
+  #  :user_name            => ENV['gmail_username'],
+  #  :password             => ENV['gmail_password'],
+  #  :authentication       => "plain",
+  # :enable_starttls_auto => true
+  # }
+  config.active_record.dump_schema_after_migration = false
+  # added by Misley
+  config.action_mailer.default_url_options = { :host => 'standupsessions.herokuapp.com' }
   config.action_mailer.delivery_method = :smtp
-  # SMTP settings for gmail
+  config.action_mailer.perform_deliveries = true
+  config.action_mailer.raise_delivery_errors = false
+  config.action_mailer.default :charset => "utf-8"
   config.action_mailer.smtp_settings = {
-   :address              => "smtp.gmail.com",
-   :port                 => 587,
-   :user_name            => ENV['gmail_username'],
-   :password             => ENV['gmail_password'],
-   :authentication       => "plain",
-  :enable_starttls_auto => true
+    address: "smtp.gmail.com",
+    port: 587,
+    domain: "standupsessions.herokuapp.com",
+    authentication: "plain",
+    enable_starttls_auto: true,
+    user_name: ENV["GMAIL_USERNAME"],
+    password: ENV["GMAIL_PASSWORD"]
   }
+
 
   config.action_mailer.perform_deliveries = true
 
