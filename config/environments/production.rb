d37b064b1ab42a55d6fb5959475a1f17c0d910f2
Santiago Garza <sgarzaceja@me.com>
2016-06-13 13:38:42 -0400

    mailer

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -77,18 +77,27 @@ Rails.application.configure do
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
   # added by Misley
-  config.action_mailer.default_url_options = { :host => 'standupsessions.herokuapp.com' }
-  config.action_mailer.delivery_method = :smtp
-  config.action_mailer.perform_deliveries = true
-  config.action_mailer.raise_delivery_errors = false
-  config.action_mailer.default :charset => "utf-8"
-  config.action_mailer.smtp_settings = {
-    address: "smtp.gmail.com",
-    port: 587,
-    domain: "standupsessions.herokuapp.com",
-    authentication: "plain",
-    enable_starttls_auto: true,
-    :user_name => ENV[‘SENDGRID_USERNAME’],
-    :password => ENV[‘SENDGRID_PASSWORD’],
-  }
-end
+#   config.action_mailer.default_url_options = { :host => 'standupsessions.herokuapp.com' }
+#   config.action_mailer.delivery_method = :smtp
+#   config.action_mailer.perform_deliveries = true
+#   config.action_mailer.raise_delivery_errors = false
+#   config.action_mailer.default :charset => "utf-8"
+#   config.action_mailer.smtp_settings = {
+#     address: "smtp.gmail.com",
+#     port: 587,
+#     domain: "standupsessions.herokuapp.com",
+#     authentication: "plain",
+#     enable_starttls_auto: true,
+#     user_name: ENV["gmail_username"],
+#     password: ENV["gmail_password"]
+#   }
+# end
+ActionMailer::Base.smtp_settings = {
+:address => ‘smtp.gmail.com’,
+:port => ‘587’,
+:authentication => :plain,
+:user_name => ENV[‘SENDGRID_USERNAME’],
+:password => ENV[‘SENDGRID_PASSWORD’],
+:domain => ‘standupsessions.herokuapp.com’
+}
+ActionMailer::Base.delivery_method = :smtp
