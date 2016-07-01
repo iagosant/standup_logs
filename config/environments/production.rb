b7e4221dd7f6ccdf4776bb0e63978b558cc4bdd6
Santiago Garza <sgarzaceja@me.com>
2016-06-30 21:34:53 -0400

    changed paperclip on production environment to work with heroku

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -1,4 +1,6 @@
 Rails.application.configure do
+
+  Paperclip.options[:command_path] == '/opt/local/bin'
   # Settings specified here will take precedence over those in config/application.rb.
 
   # Code is not reloaded between requests.
