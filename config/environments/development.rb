7f42f91c6f2822b525b8d2f4a4f13e74064ebf34
admin <admin@iMac-2.local>
2016-07-06 14:18:17 -0400

    S3

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -71,7 +71,15 @@ Rails.application.configure do
   # Raises error for missing translations
   # config.action_view.raise_on_missing_translations = true
   # Paperclip.options[:command_path] == "/opt/local/bin"
-
+  config.paperclip_defaults = {
+    storage: :s3,
+    s3_credentials: {
+      bucket: ENV.fetch('S3_BUCKET_NAME'),
+      access_key_id: ENV.fetch('AWS_ACCESS_KEY_ID'),
+      secret_access_key: ENV.fetch('AWS_SECRET_ACCESS_KEY'),
+      s3_region: ENV.fetch('AWS_REGION'),
+    }
+  }
   # config.paperclip_defaults = {
   #   :storage => :s3,
   #   :s3_credentials => {
@@ -82,7 +90,7 @@ Rails.application.configure do
   #   :url => ':s3-website-us-west-2',
   #   :path => "/:class/:attachment/:id_partition/:style/:filename"
   # }
-  
+
 
   # Paperclip.options[:command_path] = "/usr/local/bin/"
 
