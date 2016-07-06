9b164bbb71dc48211005c5bd12766cb0a35ea85f
admin <admin@iMac-2.local>
2016-07-06 16:32:03 -0400

    change

--- config/initializers/paperclip.rb
+++ config/initializers/paperclip.rb
@@ -11,10 +11,15 @@
 # Paperclip::Attachment.default_options[:bucket] = ENV['S3_BUCKET_NAME']
 # config/initializers/paperclip.rb
   # only use Amazon S2 on production servers
-  unless Rails.env.development?
-    Paperclip::Attachment.default_options[:url] = ':s3_domain_url'
-    Paperclip::Attachment.default_options[:path] = '/:class/:attachment/:id_partition/:style/:filename'
+  Paperclip::Attachment.default_options[:storage] = :s3
+Paperclip::Attachment.default_options[:s3_protocol] = 'http'
+Paperclip::Attachment.default_options[:s3_credentials] =
+  { :bucket => ENV['S3_BUCKET_NAME'],
+    :access_key_id => ENV['AWS_ACCESS_KEY_ID'],
+    :secret_access_key => ENV['AWS_SECRET_ACCESS_KEY'] }
 
   # # config/initializers/paperclip.rb
   # Paperclip::Attachment.default_options[:s3_host_name] = 's3-us-west-2.amazonaws.com'
+  Paperclip::Attachment.default_options[:url] = ':us-standard.amazonaws.com'
+  Paperclip::Attachment.default_options[:path] = '/:class/:attachment/:id_partition/:style/:filename'
   end
